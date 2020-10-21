df906754bdd1ecfa3c6fbf08abfb1319975bd0d6
mforstchen <71297558+mforstchen@users.noreply.github.com>
2020-10-21 17:21:39 -0400

    Update Forstchen_Meghan_Homework8.R
    Question 2 edits

--- Forstchen_Meghan_Homework8.R
+++ Forstchen_Meghan_Homework8.R
@@ -50,39 +50,47 @@ plot(cumulative_scores$Time, cumulative_scores$Cumulative.UW.Score, type = "l",
 
 #Question 2
 
+#IMPORTANT NOTE FOR RUNNING GAME: RUN EACH LINE IN THE SCRIPT ONE AT A TIME!
+
+
 #Builds a "Guess a Number Game" with the following rules
 #Number to guess will be generated from numbers between 1:100
 #User will be able to guess up to 10 times
 
 #Maximum number of guesses made
 MaximumGuess=10
-
+#Create a count to keep track of guesses made
+Count=0
 #The random number is generated using random integer function
 Number <- sample(1:100, 1)
 
 
-#Ask the user to guess the number
-cat("I am thinking of a number between 1 and 100. What is it?")
-
 # Solicit input from the user
+Input <- as.numeric(readline(prompt="Enter Number: "))
 
-Input <- readline(prompt="Enter Number: ")
-
-
-for(i in 1:MaximumGuess) {
-  if(Input == Number) {
-    cat("You got the correct number!")
-  } else {        
-    if(i == MaximumGuess) {
-      cat("Too bad, you ran out of guesses!")
-    } else {
-      if(Input < Number) {
-        cat("Too low! Guess again!")
+while(Count<10){
+  
+  for(i in 1:MaximumGuess) {
+    if(Input== Number) {
+      cat("You got the correct number!")
+      Count = Count+11
+      break
+    } else {        
+      if(i == MaximumGuess) {
+        cat("Too bad, you ran out of guesses!")
+        Count = Count+1
+        break
       } else {
-        cat("You are too high. Guess again!")
-      }
-      Input <- readline(prompt="Enter Number: ")
-    }            
+        if(Input < Number) {
+          cat("Too low! Guess again!")
+          Count=Count+1
+        } else {
+          cat("You are too high. Guess again!")
+          Count=Count+1
+        }
+        #Ask the user to guess the number
+        Input <- as.numeric(readline(prompt="Enter Number: "))
+      }            
+    }
   }
 }
-
