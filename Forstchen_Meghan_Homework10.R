dfd31e05d8d6a4dedccaae93e33019dc7d99bc3e
mforstchen <71297558+mforstchen@users.noreply.github.com>
2020-10-20 22:00:00 -0400

    Update Forstchen_Meghan_Homework10.R
    Figured out cumulative scores in question 1!

--- Forstchen_Meghan_Homework10.R
+++ Forstchen_Meghan_Homework10.R
@@ -1,6 +1,5 @@
 #Question 1
 #set working directory to location of files for tutorial
-
 setwd("~/Desktop/Biocomputing2020_Tutorial10/")
 
 #Read in the UWvMSU_1-22-13 text file as a dataframe called 
@@ -9,20 +8,35 @@ UWvMSU<- read.csv("UWvMSU_1-22-13.txt", sep = "", header = TRUE, stringsAsFactor
 #Determine the number of rows necessary for the dataframe
 nrow(UWvMSU)
 
-#Preallocate a dataframe for cumulative scores
+#Preallocate a dataframe for time, scores, and cumulative scores
 cumulative_scores <- data.frame("Time" = numeric(50),
+                                "UW Score" = numeric(50),
+                                "MSU Score" = numeric(50),
                                 "Cumulative UW Score" = numeric(50),
                                 "Cumulative MSU Score" = numeric(50))
 
-#Create a for loop
 
-for (i in 1:nrow(Scores)){
-  if(Scores$team[i]=="UW"){
-    UW_Score_Sum=UW_Score_Sum+cumsum(Scores$score[i])
+#Enter time from UWvMSU dataframe into cumulative_scores
+cumulative_scores$Time <- UWvMSU$time
+
+#Get the scores from original table and assign them to their team
+for(i in 1:nrow(UWvMSU)){
+  if(UWvMSU$team[i]=="UW"){
+    cumulative_scores$UW.Score[i]=(UWvMSU$score[i])
   }else{
-    MSU_Score_Sum=MSU_Score_Sum+cumsum(Scores$score[i])
+    cumulative_scores$MSU.Score[i]=(UWvMSU$score[i])
   }
-  }
+}
+
+#Now loop through and calculate cumulative scores for UW
+for(i in 1:nrow(cumulative_scores)){
+  cumulative_scores$Cumulative.UW.Score[i]=sum(cumulative_scores$UW.Score[1:i])
+}
+
+#Now loop through and calculate cumulative sums for MSU
+for(i in 1:nrow(cumulative_scores)){
+  cumulative_scores$Cumulative.MSU.Score[i]=sum(cumulative_scores$MSU.Score[1:i])
+}
 
 
 #Question 2
